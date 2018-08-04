944d7d2cdc2e56b1e8b914bb8597367532c78067
sanghegde <41577414+sanghegde@users.noreply.github.com>
2018-08-04 08:18:27 +0530

    Update TestLinkedHashSet.java

--- BasicCollections/src/com/collection/TestLinkedHashSet.java
+++ BasicCollections/src/com/collection/TestLinkedHashSet.java
@@ -1,3 +1,4 @@
+//add add names to the linkedhashedset 
 package com.collection;
 
 import java.util.LinkedHashSet;
@@ -7,12 +8,12 @@ public class TestLinkedHashSet {
 	public static void main(String[] args) {
 		LinkedHashSet<String> linkedhashset=new LinkedHashSet<>();
 		
-		linkedhashset.add("Sayali");
-		linkedhashset.add("Siddhi");
-		linkedhashset.add("Nishad");
-		linkedhashset.add("Drishti");
-		linkedhashset.add("Anindya");
-		
+		linkedhashset.add("Sangeeta");
+		linkedhashset.add("Deepak");
+		linkedhashset.add("Chandrakala");
+		linkedhashset.add("Jayaprakasha");
+		linkedhashset.add("Krishnamurthy");
+		//printing the names
 		linkedhashset.stream().forEach((name) -> System.out.println(name));
 	}
 
